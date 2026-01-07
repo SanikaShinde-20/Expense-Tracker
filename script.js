4055c4dc28f7185d2e8634acb7a2bb4bdbbbf320
SanikaShinde-20 <shindesanika765@gmail.com>
2026-01-07 15:29:25 +0530

    Changes for project with html css js

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const expenseForm = document.getElementById('expense-form');
+    const expenseNameInput = document.getElementById('expense-name');
+    const expenseAmountInput = document.getElementById('expense-amount');
+    const expenseList = document.getElementById('expense-list');
+    const totalAmountDisplay = document.getElementById('total-amount');
+
+    let expenses = [];
+    let totalAmount = calculateTotal();
+
+    expenseForm.addEventListener('submit', (e) => {
+
+    })
+
+    function calculateTotal() {
+    }
+
+})
